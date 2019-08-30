57e26b7cc270382cbc16e165e6763a9adc0943d3
Hicham Janati <hicham.janati@inria.fr>
2019-08-30 23:35:08 +0200

    rebuilt docs

--- mutar/__init__.py
+++
@@ -1,6 +0,0 @@
-from .estimators import DirtyModel, GroupLasso
-from ._version import __version__
-from .solver_dirty import solver_dirty
-
-__all__ = ['DirtyModel', 'solver_dirty', 'GroupLasso',
-           '__version__']
--- mutar/utils.py
+++
@@ -1,8 +0,0 @@
-"""Utils functions."""
-import numpy as np
-
-
-def residual(X, coef_, y):
-    """Compute X dot theta."""
-    R = y - np.array([x.dot(th) for x, th in zip(X, coef_.T)])
-    return R
